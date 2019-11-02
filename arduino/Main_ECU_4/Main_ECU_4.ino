13c8d38027023ecb091fc9cbd74810cb12cd0ea5
LeHuman <47549331+LeHuman@users.noreply.github.com>
2019-11-02 02:23:18 -0500

    revaluating choices
    without hashing on arduino this won't be as fun

--- arduino/Main_ECU_4/Main_ECU_4.ino
+++ arduino/Main_ECU_4/Main_ECU_4.ino
@@ -1,15 +1,21 @@
-#include <IFCT.h> //ImprovedFLexCanLibrary
+#include <IFCT.h> // ImprovedFLexCanLibrary
 
-CAN_message_t dataIn, dataOut; //Can message obj
-
-/*     Pins    */
-//Aero
-//Electrical
-//Gyro
-//Pump
+/*     Pins    */ //TODO: add all pins for individual teensys
+// Aero
+// Electrical
+// Gyro
+// Pump
 
 /*     enums    */
-enum motor_ID : byte {
+enum dataMapType {
+    NIL,
+    FLAG,
+    CHECK,
+    LOWBYTE,
+    HIGHBYTE,
+};
+enum addresses : byte { // TODO: layout all the addresses for everything
+    // motor
     TEMP1 = 0x0A0,
     TEMP2 = 0x0A1,
     TEMP3 = 0x0A2,
@@ -19,143 +25,70 @@ enum motor_ID : byte {
     CURRENT = 0x0A6,
     VOLTAGE = 0x0A7,
     FAULT = 0x0AB,
+    // Yes
+    FOO = 0x0FF,
+    BAR = 0x0BB,
 };
-byte *motor_msgDef[6];
-enum motor_msgEnums : byte {
-    //motor_volt
-    DCBus = 0,
-    Output = 1,
-    VAB_Vd = 2,
-    VBC_Vq = 3,
-    //motor_current
-    PhaseA = 0,
-    PhaseB = 1,
-    PhaseC = 2,
-    DCBus = 3,
-    //motor_temp1
-    PhaseA = 0,
-    PhaseB = 1,
-    PhaseC = 2,
-    DriverBoard = 3,
-    //motor_temp2
-    ControlBoard = 0,
-    //motor_temp3
-    motor = 2,
-    torqueShudder = 3,
-    //motor_pos
-    angle = 0,
-    anglrVel = 1,
-    electricalFreq = 2,
-};
-// enum motor_volt : byte {
-//     DCBus,
-//     Output,
-//     VAB_Vd,
-//     VBC_Vq,
-// };
-// enum motor_current : byte {
-//     PhaseA,
-//     PhaseB,
-//     PhaseC,
-//     DCBus,
-// };
-// enum motor_temp1 : byte {
-//     PhaseA,
-//     PhaseB,
-//     PhaseC,
-//     DriverBoard,
-// };
-// enum motor_temp2 : byte {
-//     ControlBoard
-// };
-// enum motor_temp3 : byte {
-//     motor = 2,
-//     torqueShudder = 3,
-// };
-// enum motor_pos : byte {
-//     angle,
-//     anglrVel,
-//     electricalFreq,
-// };
-/*     motorFaults    */
-char *faults_decoder[8][8] =
-    {
-        {"Hardware Gate/Desaturation Fault",
-         "HW over-current Fault",
-         "Accelerator shorted",
-         "Accelerator Open",
-         "Current sensor Low",
-         "Current sensor High",
-         "Module Tempature Low",
-         "Module Tempature High"},
-        {"Control PCB Temperature Low",
-         "Control PCB Temperature High",
-         "Gate Dive PCB Temperature Low",
-         "Gate Dive PCB Temperature High",
-         "5V Sense Voltage Low",
-         "5V Sense Voltage High",
-         "12V Sense Voltage Low",
-         "12V Sense Voltage High"},
-        {"2.5V Sense Voltage Low",
-         "2.5V Sense Voltage High",
-         "1.5V Sense Voltage Low",
-         "2.5V Sense Voltage High",
-         "DC Bus Voltage High",
-         "DC Bus Voltage Low",
-         "Precharge Timeout",
-         "Precharge Voltage Failure"},
-        {"EEPROM Checksum Invalid",
-         "EEPROM Data Out of Range",
-         "EEPROM Update Required",
-         "Reserved",
-         "Reserved",
-         "Reserved",
-         "Brake Shorted",
-         "Brake Open"},
-        {"Motor Over-speed Fault",
-         "Over-current Fault",
-         "Over-voltage Fault",
-         "Inverter Over-temperature Fault",
-         "Accelerator Input Shorted Fault",
-         "Accelerator Input Open Fault",
-         "Direction Command Fault",
-         "Inverter Response Time-out Fault"},
-        {"Hardware Gate/Desaturation Fault_2",
-         "Hardware Over-current Fault_2",
-         "Under-voltage Fault",
-         "CAN Command Message Lost Fault",
-         "Motor Over-temperature Fault",
-         "Reserved",
-         "Reserved",
-         "Reserved"},
-        {"Brake Input Shorted Fault",
-         "Brake Input Open Fault",
-         "Module A Over-temperature Fault7",
-         "Module B Over-temperature Fault7",
-         "Module C Over-temperature Fault7",
-         "PCB Over-temperature Fault7",
-         "Gate Drive Board 1 Over-temperature Fault",
-         "Gate Drive Board 2 Over-temperature Fault7"},
-        {"Gate Drive Board 3 Over-temperature Fault7",
-         "Current Sensor Fault",
-         "Reserved",
-         "Reserved",
-         "Reserved",
-         "Reserved",
-         "Resolver Not Connected",
-         "Inverter Discharge Active"}};
 
+/*     structs    */
+//TODO: make motorPackets work with general dataPackets
 struct motorDataPacket {
     byte idOffset = 0;
 
-    int values[8][8]; //not all the tables will be used eg. ANLIV & DIGIS
+    int values[8][8]; // not all the tables will be used eg. ANLIV & DIGIS
     bool faults[8][8];
 
 } motor0, motor1;
+/*
+    dataMap type helps organize the data layout for
+    teensy to teesnsy transfer through CANbus
+*/
+typedef struct dataMap {
+    byte address;
+    bool map[8];
+    // char *decode[4]; // TODO: figure out how decoding what is what will work, T2T does not need decoding only the address
+    // byte offset = 0; // implement if packets need ability offset eg. duplicate sensors
+} dataMap;
 
-void setup() {
-    //load all the zeros for the motors
-    for (int j = 0; j < 8; ++j) {
+/*     dataMaps    */ // preferably, LOWBYTE come before a HIGHBYTE
+dataMap fooMap = {
+    FOO,
+    {
+        FLAG,
+        LOWBYTE,
+        HIGHBYTE,
+        NIL,
+        NIL,
+        NIL,
+        NIL,
+        CHECK,
+    },
+};
+dataMap barMap = {
+    BAR,
+    {
+        FLAG,
+        LOWBYTE,
+        HIGHBYTE,
+        NIL,
+        LOWBYTE,
+        HIGHBYTE,
+        NIL,
+        CHECK,
+    },
+};
+
+dataMap *maps[] = {
+    fooMap,
+    barMap,
+}
+
+void
+setup() {
+    // Set motorID address offsets
+    motor1.idOffset = 0x30;
+    motor0.idOffset = 0x0;
+    for (int j = 0; j < 8; ++j) { // load all the zeros for the motor fields
         for (int k = 0; k < 8; ++k) {
             motor0.faults[j][k] = 0;
             motor1.faults[j][k] = 0;
@@ -163,60 +96,83 @@ void setup() {
             motor1.values[j][k] = 0;
         }
     }
-    for (size_t i = 0; i < 6; i++) {
-        motor_msgDef[i] = new byte[4];
-    }
 
-    pinMode(2, OUTPUT);        // Fusion Tech's Dual CAN-Bus R pin switch
-    Can0.setBaudRate(1000000); //Speeed
-    Can0.enableFIFO();         //FirstInFirstOut
+    pinMode(2, OUTPUT); // Fusion Tech's Dual CAN-Bus R pin switch
+    digitalWrite(2, LOW);
+    Can0.setBaudRate(1000000); // Speeed
+    Can0.enableFIFO();         // FirstInFirstOut
 }
 
 void loop() {
+    CAN_message_t dataIn; // Can message obj
     if (Can0.read(dataIn)) {
-        //check if id is within motor info id range
-        if (byte pos = map(dataIn.id, TEMP1, VOLTAGE, 0, 8); pos <= 7) { //TODO: ensure ids are always pos
-            checkMotorPacket(motor0.values[pos], motor_msgDef[pos]);
-            checkMotorPacket(motor1.values[pos], motor_msgDef[pos]);
-        } else if (dataIn.id == FAULT) {
-            checkFault(motor0.faults);
-            checkFault(motor1.faults);
+        if (readMotor(dataIn, motor0)) {
+            // Can0.write(dataOut);
+        } else if (readMotor(dataIn, motor1)) {
+            // Can0.write(dataOut);
+        } else {
+            for (dataMap *map : maps) {
+                if ((*map).address == dataIn.id) {
+
+                    break;
+                };
+            }
         }
     }
 }
 
-void checkMotorPacket(int valueTbl[], byte msgDef[]) {
-    // byte low_byte = position_1 * 2;
-    // byte high_byte = low_byte + 1;
-    // int value = pre_data; // Value set so the number never changes when ID is not true
-    // if (RX_msg.id == ID) // ID of motor array
-    // {
-
-    for (int i = 0; i < 4; i++) {
-        valueTbl[i] = 5;
+void pushCAN(const byte address, const char position, const uint8_t value[]) {
+    // CAN value conversion : value = (highByte x 256) + lowByte
+    CAN_message_t dataOut; // Can message obj
+    dataOut.ext = 0;
+    dataOut.id = address;
+    dataOut.len = 8;
+    uint8_t c = 0;
+    for (uint8_t i = 0; i < 8; i += 2) {
+        c += i / 2;
+        dataOut.buf[i] = value[c] % 256;     // lowByte
+        dataOut.buf[i + 1] = value[c] / 256; // highByte
     }
-
-    // long full_data = (RX_msg.buf[high_byte] * 255) + RX_msg.buf[low_byte];
-    // if (RX_msg.buf[high_byte] < 128) {
-    //     value = full_data;
-    // } else if (RX_msg.buf[high_byte] > 128) {
-    //     value = map(full_data, 65280, 32640, 0, -32640);
-    // }
-    // }
 }
 
-void checkFault(bool faultTbl[8][8]) {
-    for (int col = 0; col < 8; ++col) // for each byte
-    {
-        if (dataIn.buf[col]) // If the byte has info
-        {
-            for (int row = 0; row < 8; ++row) // for each bit
-            {
-                if (((dataIn.buf[col] >> row) & 0B00000001)) // If each bit is true, store value
-                {
+//TODO: figure out how data will be pushed to andriod
+void pushSerial(const CAN_message_t &packet) {
+    // Serial.write();
+}
+
+bool readMotor(const CAN_message_t &dataIn, motorDataPacket &packet) {
+    uint_fast8_t offst = packet.idOffset; //weird unsigned comparison issues?
+    // use map to check if id is within motor id range
+    byte pos = map(dataIn.id, TEMP1 + offst, VOLTAGE + offst, 0, 8);
+    if (pos <= 7) {
+        readMotorPacket(dataIn, packet.values[pos]);
+        return true;
+    } else if (dataIn.id == FAULT + offst) {
+        readFault(dataIn, packet.faults);
+    }
+    return false;
+}
+
+void readMotorPacket(const CAN_message_t &dataIn, int *valueTbl) {
+    for (size_t i = 0; i < 4; i++) {
+        byte lowByte = i * 2;
+        byte highByte = lowByte + 1;
+        long full_data = (dataIn.buf[highByte] * 255) + dataIn.buf[lowByte];
+        if (dataIn.buf[highByte] < 128) {
+            valueTbl[i] = full_data;
+        } else if (dataIn.buf[highByte] > 128) {
+            valueTbl[i] = map(full_data, 65280, 32640, 0, -32640);
+        }
+    }
+}
+
+void readFault(const CAN_message_t &dataIn, bool faultTbl[8][8]) {
+    for (int col = 0; col < 8; ++col) {                        // for each byte
+        if (dataIn.buf[col]) {                                 // If the byte has info
+            for (int row = 0; row < 8; ++row) {                // for each bit
+                if (((dataIn.buf[col] >> row) & 0B00000001)) { // If each bit is true, store value
                     faultTbl[col][row] = 1;
-                } else // No fault
-                {
+                } else { // No fault
                     faultTbl[col][row] = 0;
                 }
             }
